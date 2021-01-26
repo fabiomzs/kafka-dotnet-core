fc5cebdd39c7204162e6899fc3ffdbbf8cab4201
fabiomzs <fabio.muniz@outlook.com>
2021-01-26 00:16:59 -0300

    First commit

---
+++ FabioMuniz.Kafka.Sample.Consumer/Program.cs
@@ -0,0 +1,58 @@
+﻿using Confluent.Kafka;
+using FabioMuniz.Kafka.Sample.Domain;
+using Newtonsoft.Json;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FabioMuniz.Kafka.Sample.Consumer
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            KafkaConsumer();
+        }
+
+        static void KafkaConsumer()
+        {
+            string bootstrapServer = "localhost:9092";
+            string topicName = "sample-kafka";
+
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = bootstrapServer,
+                GroupId = "sample",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            Console.CancelKeyPress += Console_CancelKeyPress;
+
+            try
+            {
+                using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+                {
+                    consumer.Subscribe(topicName);
+
+                    while(true)
+                    {
+                        Order order = JsonConvert.DeserializeObject<Order>(consumer.Consume(cts.Token).Message.Value);
+
+                        Console.WriteLine($"ID: {order.Id}, QT: {order.Quantity}, VL: {order.Value}, CP: {order.Company}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message} ---> {ex?.InnerException?.Message}");
+            }
+        }
+
+        private static void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e)
+        {
+            e.Cancel = true;
+            (sender as CancellationTokenSource).Cancel();
+        }
+    }
+}
---
+++ FabioMuniz.Kafka.Sample.Producer/Program.cs
@@ -0,0 +1,50 @@
+﻿using Confluent.Kafka;
+using FabioMuniz.Kafka.Sample.Domain;
+using Newtonsoft.Json;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FabioMuniz.Kafka.Sample.Producer
+{
+    static class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Random random = new Random();
+            Order order;
+
+            for (int i = 0; i < 200; i++)
+            {
+                order = new Order(random.Next(1, 10), random.Next(100, 1200), "Fabio Muniz");
+
+                await KafkaProducerAsync(JsonConvert.SerializeObject(order));
+            }
+        }
+
+        static async Task KafkaProducerAsync(string message)
+        {
+            string bootstrapServer = "localhost:9092";
+            string topicName = "sample-kafka";
+
+            try
+            {
+                var config = new ProducerConfig
+                {
+                    BootstrapServers = bootstrapServer
+                };
+
+                using (var producer = new ProducerBuilder<Null, string>(config).Build())
+                {
+                    var result = await producer.ProduceAsync(topicName, new Message<Null, string> { Value = message });
+                    Thread.Sleep(500);
+                    Console.WriteLine(result.Message.Value);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message} ---> {ex?.InnerException?.Message}");
+            }
+        }
+    }
+}
